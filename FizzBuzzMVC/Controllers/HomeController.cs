b02527018965013bae694a83e7e48841fe983fac
sam-pixelbuilders <sam@pixelbuilders.com>
2022-03-09 08:42:30 +0000

    Added FizzBuzzMVC to repo

---
+++ FizzBuzzMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using FizzBuzzMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FizzBuzzMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            FizzBuzzViewModel fbModel = new FizzBuzzViewModel()
+            {
+                Numbers = GetResults(),
+                FizzAmount = CalculateFizzAmount(),
+                BuzzAmount = CalculateBuzzAmount(),
+                WoofAmount = CalculateWoofAmount()
+            };
+            
+            return View(fbModel);
+        }
+
+        private string OutputString(int value)
+        {
+            var returnStr = string.Format($"{CheckForFizz(value)}{CheckForBuzz(value)}{CheckForWoof(value)}");
+            return !string.IsNullOrEmpty(returnStr) ? returnStr : value.ToString();
+        }
+
+        private string CheckForFizz(int value) => value % 3 == 0 ? "Fizz" : string.Empty;
+
+        private string CheckForBuzz(int value) => value % 5 == 0 ? "Buzz" : string.Empty;
+
+        private string CheckForWoof(int value) => value % 7 == 0 ? "Woof" : string.Empty;
+
+        private List<string> GetResults()
+        {
+            var fbNumbers = new List<string>();
+            Enumerable.Range(1, 100).ToList().ForEach(i => fbNumbers.Add(OutputString(i)));
+            return fbNumbers;
+        }
+
+        private int CalculateFizzAmount() => (100 / 3);
+        private int CalculateBuzzAmount() => (100 / 5);
+        private int CalculateWoofAmount() => (100 / 7);
+    }
+}
---
+++ FizzBuzzMVC/Models/FizzBuzzViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FizzBuzzMVC.Models
+{
+    public class FizzBuzzViewModel
+    {
+        public FizzBuzzViewModel()
+        {
+
+        }
+        public FizzBuzzViewModel(List<string> numbers, int fizzAmount, int buzzAmount, int woofAmount)
+        {
+            Numbers = numbers;
+            FizzAmount = fizzAmount;
+            BuzzAmount = buzzAmount;
+            WoofAmount = woofAmount;
+        }
+
+        public List<string> Rules { get; set; }
+        public List<string> Numbers { get; set; }
+        public int FizzAmount { get; set; }
+        public int BuzzAmount { get; set; } 
+        public int WoofAmount { get; set; }
+    }
+}
